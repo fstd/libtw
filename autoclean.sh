3bec4106c05379743ce3ff2282e6e94c94d7fe8e
fisted <van.fisted@googlemail.com>
2013-06-16 23:03:59 +0200

    c++11

--- autoclean.sh
+++ autoclean.sh
@@ -5,7 +5,11 @@ rm -f Makefile.in
 rm -f aclocal.m4
 rm -rf autom4te.cache/
 rm -rf build-aux
-rm -rf m4
+rm -f m4/libtool.m4
+rm -f 'm4/lt~obsolete.m4'
+rm -f m4/ltoptions.m4
+rm -f m4/ltsugar.m4
+rm -f m4/ltversion.m4
 rm -f config.h.in
 rm -f 'config.h.in~'
 rm -f configure
@@ -13,3 +17,5 @@ rm -f include/Makefile.in
 rm -f include/libtw/Makefile.in
 rm -f libtw/Makefile.in
 rm -f src/Makefile.in
+rm -f configure.scan
+rm -f autoscan-2.69.log
