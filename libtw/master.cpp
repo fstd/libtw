3768c540b1cd4fa76d53c31c67bd2e295ae3aee6
fisted <van.fisted@googlemail.com>
2013-06-16 02:23:18 +0200

    MasterComm implementation stubs

---
+++ libtw/master.cpp
@@ -0,0 +1,29 @@
+/* master.cpp - (C) 2013, Timo Buhrmester, Learath2
+ * libtw - uhm
+  * See README for contact-, COPYING for license information.  */
+
+#include <cstdio>
+
+#include <libtw/master.h>
+
+namespace tw {
+
+MasterComm::MasterComm()
+{
+}
+
+MasterComm::~MasterComm()
+{
+}
+
+void
+MasterComm::SetMasters(vector<string> const& masters)
+{
+}
+
+void
+MasterComm::GetList(vector<string> & result)
+{
+}
+
+};
