2573f39fd72de79e12e71217042501319f8a03ee
fisted <van.fisted@googlemail.com>
2013-06-16 23:06:46 +0200

    add some magic to pktgen

--- include/libtw/pktgen.h
+++ include/libtw/pktgen.h
@@ -6,14 +6,46 @@
 #define LIBTW_PKTGEN_H
 
 #include <cstddef>
+#define Y(A...) A
 
 namespace tw {
+
+
+#define CONNLESS_PACKETS                                        \
+X(SB_HEARTBEAT,        Y({255, 255, 255, 255, 'b', 'e', 'a', '2'})) \
+X(SB_GETLIST,          Y({255, 255, 255, 255, 'r', 'e', 'q', '2'})) \
+X(SB_LIST,             Y({255, 255, 255, 255, 'l', 'i', 's', '2'})) \
+X(SB_GETCOUNT,         Y({255, 255, 255, 255, 'c', 'o', 'u', '2'})) \
+X(SB_COUNT,            Y({255, 255, 255, 255, 's', 'i', 'z', '2'})) \
+X(SB_GETINFO,          Y({255, 255, 255, 255, 'g', 'i', 'e', '3'})) \
+X(SB_INFO,             Y({255, 255, 255, 255, 'i', 'n', 'f', '3'})) \
+X(SB_FWCHECK,          Y({255, 255, 255, 255, 'f', 'w', '?', '?'})) \
+X(SB_FWRESPONSE,       Y({255, 255, 255, 255, 'f', 'w', '!', '!'})) \
+X(SB_FWOK,             Y({255, 255, 255, 255, 'f', 'w', 'o', 'k'})) \
+X(SB_FWERROR,          Y({255, 255, 255, 255, 'f', 'w', 'e', 'r'})) \
+X(SB_HEARTBEAT_LEGACY, Y({255, 255, 255, 255, 'b', 'e', 'a', 't'})) \
+X(SB_GETLIST_LEGACY,   Y({255, 255, 255, 255, 'r', 'e', 'q', 't'})) \
+X(SB_LIST_LEGACY,      Y({255, 255, 255, 255, 'l', 'i', 's', 't'})) \
+X(SB_GETCOUNT_LEGACY,  Y({255, 255, 255, 255, 'c', 'o', 'u', 'n'})) \
+X(SB_COUNT_LEGACY,     Y({255, 255, 255, 255, 's', 'i', 'z', 'e'})) \
+X(VS_GETVERSION,       Y({255, 255, 255, 255, 'v', 'e', 'r', 'g'})) \
+X(VS_VERSION,          Y({255, 255, 255, 255, 'v', 'e', 'r', 's'})) \
+X(VS_GETMAPLIST,       Y({255, 255, 255, 255, 'v', 'm', 'l', 'g'})) \
+X(VS_MAPLIST,          Y({255, 255, 255, 255, 'v', 'm', 'l', 's'})) \
+X(INVALID_CLPKT,       Y({254, 255, 255, 255, 'i', 'n', 'v', 'l'}))
+
+#define X(a, b) a,
+enum EClPkts {
+	CONNLESS_PACKETS
+};
+#undef X
+
 class PktGen {
 public:
 	PktGen();
 	virtual ~PktGen();
 
-	size_t MkConnless(unsigned char *pBuf, size_t BufSz,
+	size_t MkConnless(unsigned char *pBuf, size_t BufSz, EClPkts typ,
 			const void *pData, size_t DataLen);
 
 	/* tell apart connless from regular packets */
@@ -26,14 +58,18 @@ public:
 	bool IsSystem(unsigned char *pk, size_t pklen) const;
 
 	/* for connless packets, return type of connless packet */
-	int IdentifyConnless(unsigned char *pk, size_t pklen) const;
+	EClPkts IdentifyConnless(unsigned char *pk, size_t pklen) const;
 
 	/* for control-regular, return type control packet */
 	int IdentifyControl(unsigned char *pk, size_t pklen) const;
 
 	/* for normal-regular (user and system), return msgtype */
 	int IdentifyRegular(unsigned char *pk, size_t pklen) const;
+
+	const char *NameConnless(EClPkts typ) const;
+
 };
+
 };
 
 #endif /* LIBTW_PKTGEN_H */
--- libtw/pktgen.cpp
+++ libtw/pktgen.cpp
@@ -2,6 +2,8 @@
  * libtw - uhm
   * See README for contact-, COPYING for license information.  */
 
+#include <initializer_list>
+
 #include <cstring>
 
 #include <err.h>
@@ -9,6 +11,26 @@
 #include <libtw/pktgen.h>
 
 namespace tw {
+
+#define X(a, b) b,
+static const unsigned char clpkt_data[][8] = {
+	CONNLESS_PACKETS
+};
+#undef X
+
+#define X(a, b) 8,
+static const size_t clpkt_len[] = {
+	CONNLESS_PACKETS
+};
+#undef X
+
+#define X(a, b) #a,
+static const char *clpkt_names[] = {
+	CONNLESS_PACKETS
+};
+#undef X
+
+
 
 PktGen::PktGen()
 {
@@ -19,56 +41,78 @@ PktGen::~PktGen()
 }
 
 size_t
-PktGen::MkConnless(unsigned char *pBuf, size_t BufSz,
-			const void *pData, size_t DataLen)
+PktGen::MkConnless(unsigned char *buf, size_t bufsz, EClPkts typ,
+			const void *extra, size_t extralen)
 {
-	if (DataLen+6 > BufSz) {
+	size_t pklen = clpkt_len[typ];
+	if (6 + pklen + extralen > bufsz) {
 		warnx("buffer too small for connless packet");
 		return 0;
 	}
 
-	memcpy(pBuf+6, pData, DataLen);
-	for(size_t i = 0; i < 6; i++)
-		pBuf[i] = 0xff;
+	memcpy(buf+6, clpkt_data[typ], pklen);
+	if (extralen)
+		memcpy(buf+6+pklen, extra, extralen);
 
-	return DataLen+6;
+	for(size_t i = 0; i < 6; i++)
+		buf[i] = 0xff;
+
+	return 6 + pklen + extralen;
 }
 
 
 bool
 PktGen::IsConnless(unsigned char *pk, size_t pklen) const
 {
-	return false;
+	unsigned char head[] = {0xff, 0xff, 0xff, 0xff, 0xff, 0xff};
+	return memcmp(pk, head, 6) == 0;
 }
 
 bool
 PktGen::IsControl(unsigned char *pk, size_t pklen) const
 {
+	/* TODO implement me */
 	return false;
 }
 
 bool
 PktGen::IsSystem(unsigned char *pk, size_t pklen) const
 {
+	/* TODO implement me */
 	return false;
 }
 
-int
+EClPkts
 PktGen::IdentifyConnless(unsigned char *pk, size_t pklen) const
 {
-	return -1;
+	if (pklen < 14)
+		return INVALID_CLPKT;
+#define X(a, b) { unsigned char tmp[] b; \
+                  if (memcmp(tmp, pk+6, 8) == 0) \
+                          return a; }
+	CONNLESS_PACKETS
+#undef X
+	return INVALID_CLPKT;
 }
 
 int
 PktGen::IdentifyControl(unsigned char *pk, size_t pklen) const
 {
+	/* TODO implement me */
 	return -1;
 }
 
 int
 PktGen::IdentifyRegular(unsigned char *pk, size_t pklen) const
 {
+	/* TODO implement me */
 	return -1;
+}
+
+const char*
+PktGen::NameConnless(EClPkts typ) const
+{
+	return clpkt_names[typ];
 }
 
 };
