85b3c3b5d64fa5b339c30c671ae38f1713287ee0
fisted <van.fisted@googlemail.com>
2013-06-16 15:43:42 +0200

    start PkgGen

---
+++ include/libtw/pktgen.h
@@ -0,0 +1,16 @@
+/* pktgen.h - (C) 2013, Timo Buhrmester
+ * libtw - uhm
+  * See README for contact-, COPYING for license information.  */
+
+#ifndef LIBTW_PKTGEN_H
+#define LIBTW_PKTGEN_H
+
+namespace tw {
+class PktGen {
+public:
+	PktGen();
+	virtual ~PktGen();
+};
+};
+
+#endif /* LIBTW_PKTGEN_H */
---
+++ libtw/pktgen.cpp
@@ -0,0 +1,17 @@
+/* pktgen.cpp - (C) 2013, Timo Buhrmester
+ * libtw - uhm
+  * See README for contact-, COPYING for license information.  */
+
+#include <libtw/pktgen.h>
+
+namespace tw {
+
+PktGen::PktGen()
+{
+}
+
+PktGen::~PktGen()
+{
+}
+
+};
