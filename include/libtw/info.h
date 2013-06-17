54e3feb71029fbf4e819a69419c727ad9b455418
fisted <van.fisted@googlemail.com>
2013-06-17 06:11:07 +0200

    start InfoComm

---
+++ include/libtw/info.h
@@ -0,0 +1,15 @@
+/* info.h - (C) 2013, Timo Buhrmester
+ * libtw - uhm
+  * See README for contact-, COPYING for license information.  */
+
+#ifndef LIBTW_INFO_H
+#define LIBTW_INFO_H 1
+
+namespace tw {
+class InfoComm {
+public:
+	InfoComm();
+};
+};
+
+#endif /* LIBTW_INFO_H */
---
+++ libtw/info.cpp
@@ -0,0 +1,13 @@
+/* info.cpp - (C) 2013, Timo Buhrmester, Learath2
+ * libtw - uhm
+  * See README for contact-, COPYING for license information.  */
+
+#include <libtw/info.h>
+
+namespace tw {
+
+InfoComm::InfoComm()
+{
+}
+
+};
